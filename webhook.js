5710e847787a594bdea8be31341ad3a8d1267ab3
leonardomelocontact-coder <leonardomelo.contact@gmail.com>
2026-09-07 20:36:34 -0300

    Delete webhook.js

--- webhook.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (req, res) => {
-  if (req.method !== 'POST') return res.status(405).json({ error: 'Método não permitido.' });
-  console.log('Mercado Pago webhook:', JSON.stringify(req.body || {}));
-  return res.status(200).json({ received: true });
-};
